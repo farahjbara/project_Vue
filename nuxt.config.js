1a47c40032a24fb117be64127df97ad30a678546
Aysser Jbili <aisso@localhost.localdomain>
2020-07-19 18:26:21 +0100

    add jquery + semantic-ui js

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,13 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+
+    ],
+    script: [
+        { src:'/jquery.min.js' },
+        { src:'/semantic.min.js' },
+       { src:'/script.js' },
     ]
   },
   /*
